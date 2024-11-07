05f8ea4d2a0e0ada5442377de3c5c8f237f6f448
linhnvh15vn <linhnvh15vn@gmail.com>
2024-11-07 11:29:05 +0700

    chore: update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,17 @@
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "auto",
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  semi: true,
+  arrowParens: "always",
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  bracketSameLine: false,
 };
 
 export default config;
